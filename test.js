661bbb64d56f0550e6ee8495c9d574becaeca65f
hintss <hintsss@gmail.com>
2018-02-07 03:37:05 -0800

    detect "o o f"

--- test.js
+++ test.js
@@ -7,5 +7,6 @@ test('CIDR to v4 masks generate properly', t => {
   t.true(oof("OOF"), 'detects allcaps OOF');
   t.true(oof("Oof"), 'detects capitalized Oof');
   t.true(oof("lol oof asdf"), 'detects oof when not the first word');
+  t.true(oof("o o f"), 'detects oof with spaces');
   t.false(oof("proof"), 'doesn\'t detect words containing oof');
 });
